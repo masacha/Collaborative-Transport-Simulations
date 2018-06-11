6ae01adf3cbbc41092e086b22395db980457aa6f
Charlotte <charlotte.quintrand@gmail.com>
2018-06-11 21:11:33 +0900

    changement de nom : push devient bottom

--- two-dimensions/collaborative_transport_simulation.c
+++ two-dimensions/collaborative_transport_simulation.c
@@ -25,50 +25,50 @@ int main(void)
 
 /*Reference Commands*/
 
-	double x_command = 0.0; //Ne pas modifier
-	double y_command = 0.0;
-	double dx_command = 0.0; //Ne pas modifier
-	double dy_command = 0.0;
-	double f_command = 0.0; //Ne pas modifier
+	double x_bottom_command = 0.0; //Ne pas modifier
+	double y_bottom_command = 0.0;
+	double dx_bottom_command = 0.0; //Ne pas modifier
+	double dy_bottom_command = 0.0;
+	double phi_bottom_command = 0.0;
 
 /*Mobile Robot Outputs*/
 
-	double x_res_push = -0.2;
-	double dx_res_push = 0.0;
-	double ddx_res_push = 0.0;
-	double y_res_push = -1.5;
-	double dy_res_push = 0.0;
-	double ddy_res_push = 0.0;
-	double phi_res_push = Pi/2;
-	double dphi_res_push = 0.0;
-	double ddphi_res_push = 0.0;
+	double x_res_bottom = -0.2;
+	double dx_res_bottom = 0.0;
+	double ddx_res_bottom = 0.0;
+	double y_res_bottom = -1.5;
+	double dy_res_bottom = 0.0;
+	double ddy_res_bottom = 0.0;
+	double phi_res_bottom = Pi/2;
+	double dphi_res_bottom = 0.0;
+	double ddphi_res_bottom = 0.0;
 
-	double x_c_push = 0.0;
-	double y_c_push = 0.0;
+	double x_c_bottom = 0.0;
+	double y_c_bottom = 0.0;
 	
-	double l_push = 0.0;
-	double dl_push = 0.0;
-	double l_push_prev = 0.0;
+	double l_bottom = 0.0;
+	double dl_bottom = 0.0;
+	double l_bottom_prev = 0.0;
 
-	double err_x_push = 0.0;
-	double derr_x_push = 0.0;
-	double err_y_push = 0.0;
-	double derr_y_push = 0.0;
+	double err_x_bottom = 0.0;
+	double derr_x_bottom = 0.0;
+	double err_y_bottom = 0.0;
+	double derr_y_bottom = 0.0;
 
-	double f_res_push = 0.0;
-	double tau_res_push = 0.0;
+	double f_res_bottom = 0.0;
+	double tau_res_bottom = 0.0;
 
 /*Mobile Robot Inputs*/
 	
-	double ddx_comp_push = 0.0;
-	double ddy_comp_push = 0.0;
+	double ddx_comp_bottom = 0.0;
+	double ddy_comp_bottom = 0.0;
 
-	double ddx_ref_push = 0.0;
-	double ddy_ref_push = 0.0;
+	double ddx_ref_bottom = 0.0;
+	double ddy_ref_bottom = 0.0;
 
-	double ddphi_ref_push = 0.0;
+	double ddphi_ref_bottom = 0.0;
 
-	double f_dis_push = 0.0;
+	double f_dis_bottom = 0.0;
 
 /*Object*/
 	double x_o = 0.0;
@@ -83,40 +83,40 @@ int main(void)
 
 /*Motor*/
 
-	double ddtheta_ref_push_l = 0.0;
-	double ddtheta_ref_push_r = 0.0;
+	double ddtheta_ref_bottom_l = 0.0;
+	double ddtheta_ref_bottom_r = 0.0;
 	
-	double ia_ref_push_l = 0.0;
-	double ia_ref_push_r = 0.0;
+	double ia_ref_bottom_l = 0.0;
+	double ia_ref_bottom_r = 0.0;
 
-	double ia_push_l = 0.0;
-	double ia_push_r = 0.0;
+	double ia_bottom_l = 0.0;
+	double ia_bottom_r = 0.0;
 
-	double tau_dis_push_l = 0.0;
-	double tau_dis_push_r = 0.0;
+	double tau_dis_bottom_l = 0.0;
+	double tau_dis_bottom_r = 0.0;
 
-	double tau_dob_push_l = 0.0;
-	double tau_dob_push_r = 0.0;
+	double tau_dob_bottom_l = 0.0;
+	double tau_dob_bottom_r = 0.0;
 
-	double integral_tau_dob_push_l = 0.0;
-	double integral_tau_dob_push_r = 0.0;
+	double integral_tau_dob_bottom_l = 0.0;
+	double integral_tau_dob_bottom_r = 0.0;
 
-	double tau_rtob_push_l = 0.0;
-	double tau_rtob_push_r = 0.0;
+	double tau_rtob_bottom_l = 0.0;
+	double tau_rtob_bottom_r = 0.0;
 
-	double integral_tau_rtob_push_l = 0.0;
-	double integral_tau_rtob_push_r = 0.0;
+	double integral_tau_rtob_bottom_l = 0.0;
+	double integral_tau_rtob_bottom_r = 0.0;
 
-	double theta_res_push_l = 0.0;
-	double theta_res_push_r = 0.0;
+	double theta_res_bottom_l = 0.0;
+	double theta_res_bottom_r = 0.0;
 
-	double dtheta_res_push_l = 0.0;
-	double dtheta_res_push_r = 0.0;
+	double dtheta_res_bottom_l = 0.0;
+	double dtheta_res_bottom_r = 0.0;
 
-	double ddtheta_res_push_l = 0.0;
-	double ddtheta_res_push_r = 0.0;
+	double ddtheta_res_bottom_l = 0.0;
+	double ddtheta_res_bottom_r = 0.0;
 
-	l_push_prev = fabs(-sin(phi_o)*x_c_push+cos(phi_o)*y_c_push - (y_o-L_o));
+	l_bottom_prev = fabs(-sin(phi_o)*x_c_bottom+cos(phi_o)*y_c_bottom - (y_o-L_o));
 
 	FILE *fp;
 
@@ -127,106 +127,106 @@ int main(void)
 
 	while(t <= T){	
 
-		x_command = -0.2;
-		dx_command = 0.0;
-		y_command = 0.0;
-		dy_command = 0.0;
-		f_command = 0.0;
+		x_bottom_command = -0.2;
+		dx_bottom_command = 0.0;
+		y_bottom_command = 0.0;
+		dy_bottom_command = 0.0;
+		phi_bottom_command = 0.0;
 		
-		x_c_push = cos(phi_o)*x_res_push-sin(phi_o)*(y_res_push+R_r);
-		y_c_push = sin(phi_o)*x_res_push+cos(phi_o)*(y_res_push+R_r);
+		x_c_bottom = cos(phi_o)*x_res_bottom-sin(phi_o)*(y_res_bottom+R_r);
+		y_c_bottom = sin(phi_o)*x_res_bottom+cos(phi_o)*(y_res_bottom+R_r);
 
-		if(cos(phi_o)*y_c_push-sin(phi_o)*x_c_push>y_o-L_o){
-			f_dis_push = K_o*l_push + D_o*dl_push; 
-			tau_dis_push_l = f_dis_push*R_w*(1-sin(phi_res_push-phi_o))/2;
-			tau_dis_push_r = f_dis_push*R_w*(1+sin(phi_res_push-phi_o))/2;
+		if(cos(phi_o)*y_c_bottom-sin(phi_o)*x_c_bottom>y_o-L_o){
+			f_dis_bottom = K_o*l_bottom + D_o*dl_bottom; 
+			tau_dis_bottom_l = f_dis_bottom*R_w*(1-sin(phi_res_bottom-phi_o))/2;
+			tau_dis_bottom_r = f_dis_bottom*R_w*(1+sin(phi_res_bottom-phi_o))/2;
 		}
 		else{
-			f_dis_push = 0.0;
-			tau_dis_push_l = 0.0;
-			tau_dis_push_r = 0.0;
+			f_dis_bottom = 0.0;
+			tau_dis_bottom_l = 0.0;
+			tau_dis_bottom_r = 0.0;
 		}
 		
 		/*Mobile Robot Controller*/
 
-		err_x_push = x_command - L_o - x_res_push;
-		derr_x_push = dx_command - dx_res_push;
+		err_x_bottom = x_bottom_command - L_o - x_res_bottom;
+		derr_x_bottom = dx_bottom_command - dx_res_bottom;
 
-		err_y_push = y_command - L_o - y_res_push;
-		derr_y_push = dy_command - dy_res_push;
+		err_y_bottom = y_bottom_command - L_o - y_res_bottom;
+		derr_y_bottom = dy_bottom_command - dy_res_bottom;
 
-		ddx_comp_push = (K_p*err_x_push+K_v*derr_x_push)/M_c;
-		ddy_comp_push = (K_p*err_y_push+K_v*derr_y_push)/M_c;
+		ddx_comp_bottom = (K_p*err_x_bottom+K_v*derr_x_bottom)/M_c;
+		ddy_comp_bottom = (K_p*err_y_bottom+K_v*derr_y_bottom)/M_c;
 
-		ddx_ref_push = ddx_comp_push;
-		ddy_ref_push = ddy_comp_push;
+		ddx_ref_bottom = ddx_comp_bottom;
+		ddy_ref_bottom = ddy_comp_bottom;
 
-		ddphi_ref_push = 0.0;
+		ddphi_ref_bottom = 0.0;
 
 		/*Motor Controller*/
 
-		ddtheta_ref_push_l = (ddx_ref_push*cos(phi_res_push)+ddy_ref_push*sin(phi_res_push)-R_r*ddphi_ref_push)/R_w;
-		ddtheta_ref_push_r = (ddx_ref_push*cos(phi_res_push)+ddy_ref_push*sin(phi_res_push)+R_r*ddphi_ref_push)/R_w;
+		ddtheta_ref_bottom_l = (ddx_ref_bottom*cos(phi_res_bottom)+ddy_ref_bottom*sin(phi_res_bottom)-R_r*ddphi_ref_bottom)/R_w;
+		ddtheta_ref_bottom_r = (ddx_ref_bottom*cos(phi_res_bottom)+ddy_ref_bottom*sin(phi_res_bottom)+R_r*ddphi_ref_bottom)/R_w;
 
-		ia_ref_push_l = J_n*ddtheta_ref_push_l/K_tn;
-		ia_ref_push_r = J_n*ddtheta_ref_push_r/K_tn;
+		ia_ref_bottom_l = J_n*ddtheta_ref_bottom_l/K_tn;
+		ia_ref_bottom_r = J_n*ddtheta_ref_bottom_r/K_tn;
 
-		ia_push_l = ia_ref_push_l + tau_dob_push_l/K_tn;
-		ia_push_r = ia_ref_push_r + tau_dob_push_r/K_tn;
+		ia_bottom_l = ia_ref_bottom_l + tau_dob_bottom_l/K_tn;
+		ia_bottom_r = ia_ref_bottom_r + tau_dob_bottom_r/K_tn;
 
-		ddtheta_res_push_l = (K_tn*ia_push_l - tau_dis_push_l)/J_n;
-		ddtheta_res_push_r = (K_tn*ia_push_r - tau_dis_push_r)/J_n;
+		ddtheta_res_bottom_l = (K_tn*ia_bottom_l - tau_dis_bottom_l)/J_n;
+		ddtheta_res_bottom_r = (K_tn*ia_bottom_r - tau_dis_bottom_r)/J_n;
 
-		dtheta_res_push_l += ddtheta_res_push_l * ST;
-		dtheta_res_push_r += ddtheta_res_push_r * ST;
+		dtheta_res_bottom_l += ddtheta_res_bottom_l * ST;
+		dtheta_res_bottom_r += ddtheta_res_bottom_r * ST;
 
-		theta_res_push_l += dtheta_res_push_l * ST;
-		theta_res_push_r += dtheta_res_push_r * ST;
+		theta_res_bottom_l += dtheta_res_bottom_l * ST;
+		theta_res_bottom_r += dtheta_res_bottom_r * ST;
 
 		/*Robot Motion*/
-		ddx_res_push = R_w*(ddtheta_res_push_l+ddtheta_res_push_r)*cos(phi_res_push)/2;
-		dx_res_push += ddx_res_push*ST;
-		x_res_push += dx_res_push*ST;
+		ddx_res_bottom = R_w*(ddtheta_res_bottom_l+ddtheta_res_bottom_r)*cos(phi_res_bottom)/2;
+		dx_res_bottom += ddx_res_bottom*ST;
+		x_res_bottom += dx_res_bottom*ST;
 
-		ddy_res_push = R_w*(ddtheta_res_push_l+ddtheta_res_push_r)*sin(phi_res_push)/2;
-		dy_res_push += ddy_res_push*ST;
-		y_res_push += dy_res_push*ST;
+		ddy_res_bottom = R_w*(ddtheta_res_bottom_l+ddtheta_res_bottom_r)*sin(phi_res_bottom)/2;
+		dy_res_bottom += ddy_res_bottom*ST;
+		y_res_bottom += dy_res_bottom*ST;
 
-		ddphi_res_push = R_w*(ddtheta_res_push_r-ddtheta_res_push_l)/(2*R_r);
-		dphi_res_push += ddphi_res_push*ST; 
-		phi_res_push += dphi_res_push*ST;
+		ddphi_res_bottom = R_w*(ddtheta_res_bottom_r-ddtheta_res_bottom_l)/(2*R_r);
+		dphi_res_bottom += ddphi_res_bottom*ST; 
+		phi_res_bottom += dphi_res_bottom*ST;
 
 		/*Object Motion*/
 
-		ddx_o = (f_dis_push)*cos(phi_res_push)/M_o;
+		ddx_o = (f_dis_bottom)*cos(phi_res_bottom)/M_o;
 		dx_o += ddx_o*ST;
 		x_o += dx_o*ST;
-		ddy_o = (f_dis_push)*sin(phi_res_push)/M_o;
+		ddy_o = (f_dis_bottom)*sin(phi_res_bottom)/M_o;
 		dy_o += ddy_o*ST;
 		y_o += dy_o*ST;
-		ddphi_o = -f_dis_push*(cos(phi_res_push)*(x_o-x_c_push)+sin(phi_res_push)*(y_o-y_c_push))/J_o;
+		ddphi_o = -f_dis_bottom*(cos(phi_res_bottom)*(x_o-x_c_bottom)+sin(phi_res_bottom)*(y_o-y_c_bottom))/J_o;
 		dphi_o += ddphi_o*ST;
 		phi_o += dphi_o*ST;
 
-		l_push = fabs(-sin(phi_o)*x_c_push+cos(phi_o)*y_c_push - (y_o-L_o));
-		dl_push = (l_push - l_push_prev)/ST;
-		l_push_prev = l_push;
+		l_bottom = fabs(-sin(phi_o)*x_c_bottom+cos(phi_o)*y_c_bottom - (y_o-L_o));
+		dl_bottom = (l_bottom - l_bottom_prev)/ST;
+		l_bottom_prev = l_bottom;
 
 		/*disturbance observer*/		
-		tau_dob_push_l = integral_tau_dob_push_l - dtheta_res_push_l*J_n*GDIS;
-		integral_tau_dob_push_l 
-			+= ((K_tn*ia_push_l + dtheta_res_push_l*J_n*GDIS) - integral_tau_dob_push_l)*GDIS*ST;
+		tau_dob_bottom_l = integral_tau_dob_bottom_l - dtheta_res_bottom_l*J_n*GDIS;
+		integral_tau_dob_bottom_l 
+			+= ((K_tn*ia_bottom_l + dtheta_res_bottom_l*J_n*GDIS) - integral_tau_dob_bottom_l)*GDIS*ST;
 		
-		tau_dob_push_r = integral_tau_dob_push_r - dtheta_res_push_r*J_n*GDIS;
-		integral_tau_dob_push_r 
-			+= ((K_tn*ia_push_r + dtheta_res_push_r*J_n*GDIS) - integral_tau_dob_push_r)*GDIS*ST;
+		tau_dob_bottom_r = integral_tau_dob_bottom_r - dtheta_res_bottom_r*J_n*GDIS;
+		integral_tau_dob_bottom_r 
+			+= ((K_tn*ia_bottom_r + dtheta_res_bottom_r*J_n*GDIS) - integral_tau_dob_bottom_r)*GDIS*ST;
 
 		/*Mobile Robot Reaction Force*/
 
-		f_res_push = (tau_dob_push_l+tau_dob_push_r)/R_w;
-		tau_res_push = (tau_dob_push_l-tau_dob_push_r)*R_r/(R_w);
+		f_res_bottom = (tau_dob_bottom_l+tau_dob_bottom_r)/R_w;
+		tau_res_bottom = (tau_dob_bottom_l-tau_dob_bottom_r)*R_r/(R_w);
 
-		fprintf(fp, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", t, x_res_push, y_res_push, phi_res_push, x_o, y_o, phi_o, f_dis_push, f_res_push, tau_res_push);
+		fprintf(fp, "%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf\n", t, x_res_bottom, y_res_bottom, phi_res_bottom, x_o, y_o, phi_o, f_dis_bottom, f_res_bottom, tau_res_bottom);
 		
 		t += ST;
       
